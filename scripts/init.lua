de8154e9bd19264662e02e50e6196800d8846b30
Jeff Hobson <jeffrey296@gmail.com>
2023-10-03 09:18:15 -0400

    Add boilerplate and some images

---
+++ scripts/init.lua
@@ -0,0 +1,15 @@
+--Items
+Tracker:AddItems("items/items.json")
+--Logic
+-- TBD
+
+--Maps
+Tracker:AddMaps("maps/maps.json")
+--Locations
+-- TBD
+
+-- Layout
+Tracker:AddLayouts("layouts/items.json")
+Tracker:AddLayouts("layouts/levels.json")
+Tracker:AddLayouts("layouts/tabs.json")
+Tracker:AddLayouts("layouts/tracker.json")
